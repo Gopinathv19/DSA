da2ecedabd659441455373017036d23da13b00df
Gopinathv19 <gopinath5166296@gmail.com>
2024-08-02 06:18:42 +0530

    2/7/2024

---
+++ Top50Arrays/ArraySubsetOfAnotherArray.java
@@ -0,0 +1,30 @@
+package Top50Arrays;
+
+import java.util.HashMap;
+
+public class ArraySubsetOfAnotherArray {
+
+    public static void main(String[] args) {
+
+        int a1[] = {11, 7, 1, 13, 21, 3, 7, 3};
+        int a2[] = {11, 3, 7, 1, 7};
+        System.out.println(ArraySubsetOfAnotherArray(a1,a2));
+    }
+
+    public static String ArraySubsetOfAnotherArray(int[] arr1, int[] arr2){
+        HashMap<Long,Integer> FreqMap = new HashMap<>();
+        for(long element:arr1){
+             FreqMap.put(element,FreqMap.getOrDefault(element,0)+1);
+        }
+        for(long element : arr2){
+            if(FreqMap.containsKey(element) && FreqMap.get(element)>0){
+                FreqMap.put(element,FreqMap.get(element)-1);
+            }else{
+                return "No";
+            }
+        }
+
+
+        return "Yes";
+    }
+}
